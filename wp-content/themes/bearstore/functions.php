03636b1bd4f9749f735d8159064e30e9cd4b3b39
tuyenln <tuyenln@tuyenln.(none)>
2014-05-07 23:35:33 +0700

    update function

--- wp-content/themes/bearstore/functions.php
+++ wp-content/themes/bearstore/functions.php
@@ -5,8 +5,8 @@
     * @copyright Copyright (c) 2010 - 2013 JoomShaper
     * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
     */
-update_option('siteurl','http://127.0.0.1/femmefatalelashes/');
-update_option('home','http://127.0.0.1/femmefatalelashes/');
+// update_option('siteurl','http://127.0.0.1/femmefatalelashes/');
+// update_option('home','http://127.0.0.1/femmefatalelashes/');
     define('_THEME', get_template()); //Define _THEME
     define('DS', '/');
 require_once ('helix/helix.php');
