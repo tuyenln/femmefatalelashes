69708b2403ddedadad5f71508fa43e199056c8ac
TuyenLN <tuyenln@tuyenln.(none)>
2014-05-11 23:09:02 +0700

    update cate

--- wp-content/themes/bearstore/page-full.php
+++ wp-content/themes/bearstore/page-full.php
@@ -57,7 +57,7 @@ query_posts( array ( 'category_name' => 'blog', 'posts_per_page' => -1 ) );
 
 	<div class="latest-blog">
 	<?php
-		$args = array( 'numberposts' => 1 , 'category_name' => 'blog' );
+		$args = array( 'numberposts' => 1 , 'category_name' => 'promo-category' );
 		$lastposts = get_posts( $args );
 		foreach($lastposts as $post) : setup_postdata($post); ?>
 			<?php //the_content(); ?>
